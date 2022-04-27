bfc1a8b7fed116241ba7836f98e7ffb47eefbadd
juniorsn1409 <47853348+juniorsn1409@users.noreply.github.com>
2022-04-27 20:24:13 -0300

    aula10 iniciando no php

---
+++ pages/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Index</title>
+</head>
+<body>
+     <?php
+
+          $leanguage = "PHP";
+          $number =  01;
+
+          $message = "$number - my page is working with $leanguage";
+
+          echo $message;
+
+          echo "<br>";
+          echo "<br>";
+
+         $message2 = $number . " - my page is working with " . $leanguage ;
+
+         echo $message2;
+
+         echo "<br>";
+         echo "<br>";
+
+         $x = 5;
+         $y = 5;
+         $soma =  $x + $y;
+
+         $message3 = "soma de 5 + 5 = $soma" ;
+
+         echo $message3;
+
+          # evrthing is ready!
+
+          /*
+               ready to push!
+          */
+
+     ?>
+</body>
+</html>
