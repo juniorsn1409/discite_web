ce2af5769b5d0be017f72665c3feebd5e136c42f
UNASP-HT - Aluno <aluno@unasp-ht.edu.br>
2022-05-03 20:08:41 -0300

    intruções whilr e ifElse

---
+++ pages/aula11.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Aula 11</title>
+</head>
+<body>
+     <?php
+
+     $option = true;
+     $num = 1;
+
+     while(true){
+          echo"opa!! $num";
+
+          $num = $num + 1;
+
+          if($num < 10):
+               $option = true;
+          else:
+               $option = false;
+          endif;
+     }
+
+     echo"finalizado opa!";
+     ?>
+</body>
+</html>
